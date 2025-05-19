1511d139d30992876dcfb546b151f23b47eece5f
prasanna-adhikari <prasannaadhikari022@gmail.com>
2025-05-19 21:06:33 +0930

    yeeha

--- BioShield.service.worker.js
+++ BioShield.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1747654013|30472761439';
+const CACHE_VERSION = '1747654573|31032985228';
 /** @type {string} */
 const CACHE_PREFIX = 'BioShield-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
